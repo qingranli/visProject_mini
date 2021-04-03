6e4dbb28d46aeb9e9d5056ee14dccf9e4ce28e3f
Qingran Li <qingran.li@duke.edu>
2021-04-03 02:01:27 -0400

    code for plot trend (with air temp)

---
+++ Blossom_in_DC/plot_trend_with_emoji.R
@@ -0,0 +1,45 @@
+# plot time series with end point as emoji
+
+getwd()
+# setwd("./dataDir")
+
+library(data.table)
+library(tidyverse)
+library(ggplot2)
+library(ggimage)
+
+rm(list=ls())
+gc()
+
+peak <- fread("blossom_peak_date.csv")
+temp <- fread("DC_temp_Airport.csv")
+
+pt_peak <- peak$peak_date[which(peak$Year == 2021)]
+
+dt_temp <- temp %>% select(DATE,TMID) %>% 
+  mutate(Date = as.Date(DATE, format = "%m/%d/%Y")) %>%
+  mutate(Year = year(Date)) %>% 
+  filter(Year == 2021) %>% 
+  filter(Date <= pt_peak) %>%
+  select(Date,TMID)
+
+n = dim(dt_temp)[1]
+p0 <- ggplot(dt_temp, aes(x=Date,y=TMID)) + 
+  geom_path(color="mistyrose2", size = 2) + 
+  xlab("") + ylab("temperature (Celsius)") +
+  scale_x_date(date_breaks = "2 weeks",
+               date_minor_breaks = "1 week",
+               date_labels = "%b-%d",
+               limits = c(dt_temp$Date[1],dt_temp$Date[1]+133)) + 
+  theme_light() + theme(panel.grid = element_blank())
+p0
+
+# add icon as end point
+p1 <- p0 + geom_image(aes(x=dt_temp$Date[n],
+                          y = dt_temp$TMID[n],
+                          image="./cherry_blossom_icon.png"),
+                          size = 0.07, asp = 1.9) + 
+  geom_text(aes(x=dt_temp$Date[n]+7, 
+                y = dt_temp$TMID[n]-2.7,
+                label = pt_peak),color = "indianred3")
+p1
